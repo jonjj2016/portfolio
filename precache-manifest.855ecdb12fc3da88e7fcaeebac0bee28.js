2a5cc3bbe782eea8fd4307e05444817f2c2c69c4
Jon Martirosyan <jonmartirosyan1988@gmail.com>
2020-05-18 03:20:28 -0700

    Updates

---
+++ precache-manifest.855ecdb12fc3da88e7fcaeebac0bee28.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5123f36444f5f5c4ee2855f90b76f8db",
+    "url": "/MY_CV/index.html"
+  },
+  {
+    "revision": "444fa79fb3b091324ae3",
+    "url": "/MY_CV/static/css/2.de514513.chunk.css"
+  },
+  {
+    "revision": "3de7cd6a64d5769fb9c6",
+    "url": "/MY_CV/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "444fa79fb3b091324ae3",
+    "url": "/MY_CV/static/js/2.de5c9761.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/MY_CV/static/js/2.de5c9761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de7cd6a64d5769fb9c6",
+    "url": "/MY_CV/static/js/main.42c1e59e.chunk.js"
+  },
+  {
+    "revision": "e64b10066cc429316b66",
+    "url": "/MY_CV/static/js/runtime-main.8982a2b0.js"
+  },
+  {
+    "revision": "15fcb2db70705824c496e3d383d7ada6",
+    "url": "/MY_CV/static/media/1.15fcb2db.jpg"
+  },
+  {
+    "revision": "dcb55f4402e0d578390abb693189f77a",
+    "url": "/MY_CV/static/media/1.dcb55f44.jpg"
+  },
+  {
+    "revision": "01bfaa84ca4d8f42dd39aa63fabb0b5e",
+    "url": "/MY_CV/static/media/3.01bfaa84.jpg"
+  }
+]);
